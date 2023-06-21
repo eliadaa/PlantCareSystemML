c31d513462e90e220007eb18decb7ea433d841ee
eliada <bacoseliada@yahoo.ro>
2023-06-21 21:18:49 +0300

    sending min moisture value to ThingSpeak channel, to control the water pump

--- app/src/main/java/com/example/plantcaresystem/DataActivity.java
+++ app/src/main/java/com/example/plantcaresystem/DataActivity.java
@@ -20,6 +20,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,6 +34,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -179,160 +186,6 @@ public class DataActivity extends AppCompatActivity implements AdapterView.OnIte
     private float parseFloatEditText(EditText editText){
         return Float.parseFloat(editText.getText().toString());
     }
-
-//    private static DataActivity instance;
-//    public DataActivity(){
-//        instance = this;
-//    }
-
-/*
-    private void sendDataToThingSpeak(String apiKey, String value){
-
-        // Create an instance of the HttpClient class
-        HttpClient httpClient = HttpClientBuilder.create().build();
-
-        // Create an instance of the HttpPost class
-        HttpPost request = new HttpPost("https://api.thingspeak.com/update.json");
-
-        try {
-            String jsonData = "{\"api_key\":\"" + apiKey + "\",\"field5\":" + value + "}";
-
-            // Set the JSON payload as the request entity
-            StringEntity params = new StringEntity(jsonData, "UTF-8");
-            params.setContentType("application/json");
-            request.setEntity(params);
-
-            // Send the HTTP POST request to ThingSpeak
-            HttpResponse response = httpClient.execute(request);
-
-            // Process the response if needed
-            System.out.println("Response status: " + response.getStatusLine().getStatusCode());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-*/
-
-
-
-/*    private void sendDataToThingSpeak(String apiKey, String value){
-        try {
-            String urlString = "https://api.thingspeak.com/update?api_key=" + apiKey + "&field6=" + value;
-
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Data sent successfully
-                // You can handle the response if needed
-            } else {
-                // Error handling
-            }
-
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }*/
-
-/*    private void sendDataToThingSpeak(String apiKey, String value){
-            try {
-                // Create a URL object with the desired URL
-                URL url = new URL("http://www.example.com");
-
-                // Open a connection to the URL
-                URLConnection connection = url.openConnection();
-
-                // Read the content from the URL
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-                reader.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
-
-/*    public static void sendDataToThingSpeak(String url) {
-        try {
-            // Create a URL object with the desired URL
-            URL website = new URL(url);
-
-            // Open a connection to the URL
-            HttpURLConnection connection = (HttpURLConnection) website.openConnection();
-
-            // Set the request method (GET by default)
-            connection.setRequestMethod("GET");
-
-            // Connect to the URL
-            connection.connect();
-
-            // Get the response code (optional)
-            int responseCode = connection.getResponseCode();
-
-            // Disconnect/close the connection
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*//*    public static void sendDataToThingSpeak(String url) {
-        try {
-            // Create a URL object with the desired URL
-            URL website = new URL(url);
-
-            // Open a connection to the URL
-            HttpURLConnection connection = (HttpURLConnection) website.openConnection();
-
-            // Set the request method (GET by default)
-            connection.setRequestMethod("GET");
-
-            // Connect to the URL
-            connection.connect();
-
-            // Get the response code (optional)
-            int responseCode = connection.getResponseCode();
-
-            // Disconnect/close the connection
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
-
-
-/*    private void sendDataToThingSpeak(String apiKey, String value){
-        try {
-            String urlString = "https://api.thingspeak.com/update?api_key=" + apiKey + "&field6=" + value;
-//            https://api.thingspeak.com/update?api_key=VVSDJEX2MALIAU1D&field1=0
-
-            Toast.makeText(this, urlString, Toast.LENGTH_SHORT).show();
-
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Data sent successfully
-                // You can handle the response if needed
-            } else {
-                // Error handling
-            }
-
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }*/
-
-
     private void getPredefinedPlantsIntoSpinner() {
         PlantsDB.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -385,60 +238,10 @@ public class DataActivity extends AppCompatActivity implements AdapterView.OnIte
             // text_view_info.setText(actual_plant_data);
             text_view_info.setText(Html.fromHtml(actual_plant_data));
 
-/*            Float minRangeMoist = CurrentLoggedUser.getInstance().getCurrentUser().getPlant().getMinSoilMoist();
-            Toast.makeText(this, ""+ minRangeMoist, Toast.LENGTH_SHORT).show();*/
-
-/*
-            sendDataToThingSpeak("VVSDJEX2MALIAU1D", String.valueOf(minRangeMoist));
-            Toast.makeText(this, "sent", Toast.LENGTH_SHORT).show();
-
-            */
-
-
-
-
-
-
-//
-/*            if(CurrentLoggedUser.getInstance().getCurrentUser().getPlant().getMinSoilMoist() > 0){
-                // got the left limit, send this range to thingspeak -> ESP8266 to control the pump
-                sendDataToThingSpeak("VVSDJEX2MALIAU1D", Float.toString(CurrentLoggedUser.getInstance().getCurrentUser().getPlant().getMinSoilMoist()));
-                Toast.makeText(this, "sent info ", Toast.LENGTH_LONG).show();
-            }*/
+            // send min moisture value
+            sendValueToThingSpeak(String.valueOf(CurrentLoggedUser.getInstance().getCurrentUserProfile().getPlant().getMinSoilMoist()));
         }
     }
-
-/*
-    public static void sendDataToThingSpeak(String url, String value) {
-        try {
-            URL urlObject = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-
-            String postData = "field1=" + value; // Customize this based on your ThingSpeak channel
-
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(postData.getBytes());
-            outputStream.flush();
-            outputStream.close();
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-//                System.out.println("Data sent successfully to ThingSpeak.");
-                Toast.makeText(instance, "Sent data", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(instance, "Error sending data", Toast.LENGTH_LONG).show();
-//                System.out.println("Error sending data to ThingSpeak. Response Code: " + responseCode);
-            }
-
-            connection.disconnect();
-            connection.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-*/
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -513,78 +316,31 @@ public class DataActivity extends AppCompatActivity implements AdapterView.OnIte
         editTextMaxLuminosity.getText().clear();
     }
 
+    private void sendValueToThingSpeak(String value) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-/*    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String item = parent.getItemAtPosition(position).toString();
-        if(item.equals("Manual")) {
-            // user wants to manually enter data
-            // clearEditTexts
-        }
-        else {
-            PlantsDB.addListenerForSingleValueEvent(new ValueEventListener() {
+        String url = "https://api.thingspeak.com/update?api_key=TX4FWXJDY8403JQK&field1=" + value;
 
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshotSpin) {
-                    for (DataSnapshot snapshot : snapshotSpin.getChildren()) {
-                        if (snapshot.child("name").getValue(String.class).equals(item)) {
-                            editTextMyPlantName.setText(item);
-
-                            Integer minTempValue = snapshot.child("minTemp").getValue(Integer.class);
-                            Integer maxTempValue = snapshot.child("maxTemp").getValue(Integer.class);
-                            Integer minHumidityValue = snapshot.child("minHum").getValue(Integer.class);
-                            Integer maxHumidityValue = snapshot.child("maxHum").getValue(Integer.class);
-                            Integer minSoilHumidityValue = snapshot.child("minSoilHum").getValue(Integer.class);
-                            Integer maxSoilHumidityValue = snapshot.child("maxSoilHum").getValue(Integer.class);
-                            Integer minLuminosityValue = snapshot.child("minLight").getValue(Integer.class);
-                            Integer maxLuminosityValue = snapshot.child("maxLight").getValue(Integer.class);
-
-                            if (minTempValue != null)
-                                editTextMinTemp.setText(minTempValue.toString());
-                            if (maxTempValue != null)
-                                editTextMaxTemp.setText(maxTempValue.toString());
-                            if (minHumidityValue != null)
-                                editTextMinHumidity.setText(minHumidityValue.toString());
-                            if (maxHumidityValue != null)
-                                editTextMaxHumidity.setText(maxHumidityValue.toString());
-                            if (minSoilHumidityValue != null)
-                                editTextMinMoisture.setText(minSoilHumidityValue.toString());
-                            if (maxSoilHumidityValue != null)
-                                editTextMaxMoisture.setText(maxSoilHumidityValue.toString());
-                            if (minLuminosityValue != null)
-                                editTextMinLuminosity.setText(minLuminosityValue.toString());
-                            if (maxLuminosityValue != null)
-                                editTextMaxLuminosity.setText(maxLuminosityValue.toString());
-
-                            break; // Assuming there's only one match, so we can exit the loop
-                        }
+        // Request a string response from ThingSpeak min moisture channel url
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(DataActivity.this, "Sent data: " + value, Toast.LENGTH_SHORT).show();
                     }
-                }
-
-*//*                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshotSpin) {
-                    for (DataSnapshot snapshot: snapshotSpin.getChildren()) {
-                        if(snapshot.child("name").getValue(String.class).equals(item)){
-                            editTextMyPlantName.setText(item);
-                            editTextMinTemp.setText(snapshot.child("minTemp").getValue(Integer.class).toString());
-                            editTextMaxTemp.setText(snapshot.child("maxTemp").getValue(Integer.class).toString());
-                            editTextMinHumidity.setText(snapshot.child("minHum").getValue(Integer.class).toString());
-                            editTextMaxHumidity.setText(snapshot.child("maxHum").getValue(Integer.class).toString());
-                            editTextMinMoisture.setText(snapshot.child("minSoilHum").getValue(Integer.class).toString());
-                            editTextMaxMoisture.setText(snapshot.child("maxSoilHum").getValue(Integer.class).toString());
-                            editTextMinLuminosity.setText(snapshot.child("minLight").getValue(Integer.class).toString());
-                            editTextMaxLuminosity.setText(snapshot.child("maxLight").getValue(Integer.class).toString());
-                        }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(DataActivity.this, "Not sent: " + value, Toast.LENGTH_SHORT).show();
                     }
-                }*//*
+                });
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
 
-                }
-            });
-        }
-    }*/
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
